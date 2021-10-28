f46fd1d99b6069e8825573da29c524e6d832b98e
jeongbeen2 <aceman9508@gmail.com>
2021-10-28 11:37:45 +0900

    [Feat] With Logout

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,18 @@
+import { authService } from "fbase";
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Profile() {
-  return <div></div>;
+  const history = useHistory();
+  const onLogOutClick = () => {
+    signOut(authService);
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 }
